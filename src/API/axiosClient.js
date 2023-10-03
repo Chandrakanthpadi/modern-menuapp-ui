8ed493c80b851ece0ead31a0e06c87727cef8438
Chandrakanth Padi <chandrakanth.padi@gmail.com>
2023-10-04 00:07:26 +0530

    Corrected styles

--- src/API/axiosClient.js
+++ src/API/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create();
 
-axiosClient.defaults.baseURL = "http://localhost:8090/";
+axiosClient.defaults.baseURL = "http://192.168.1.5:8080/";
 
 axiosClient.defaults.headers = {
   "Content-Type": "application/json",
